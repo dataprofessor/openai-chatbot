336f8b411a3e6c783d925248511bb85dbc590b8a
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-07-24 09:18:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import openai
 import streamlit as st
 
 with st.sidebar:
-    st.title('💬🤖 OpenAI Chatbot')
+    st.title('🤖💬 OpenAI Chatbot')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
         openai.api_key = st.secrets['OPENAI_API_KEY']
